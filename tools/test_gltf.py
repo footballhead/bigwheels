4fb3a88f4c4c39bbe27c8df50c18b5d9f7ae2bc4
Michael Hitchens <hitchens@google.com>
2024-03-22 18:25:28 -0400

    Add report generation to test_gltf.py

--- tools/test_gltf.py
+++ tools/test_gltf.py
@@ -22,6 +22,37 @@ def _load_json(path: Path):
         return json.load(fd)
 
 
+def _make_report(path: Path, models_index):
+    """Generate an HTML summary of the test run with screenshot comparison"""
+
+    # TODO: Consider turning PGM to PNG here instead of during the test
+
+    report = '<html><body><table>'
+    report += '<thead><tr><th>Label</th><th>glTF-Sample-Assets Screenshot</th><th>BigWheels Screenshot</th></tr></thead>'
+    report += '<tbody>'
+    for model_spec in models_index:
+        # human-readable
+        label = model_spec['label']
+        # path in repo
+        name = model_spec['name']
+        screenshot = model_spec['screenshot']
+        variants = model_spec['variants']
+
+        for variant_name in variants:
+            variant_file = variants[variant_name]
+            report += '<tr>'
+            report += f'<td>{label} ({variant_name})</td>'
+            report += f'<td><img src="{str(_GLTF_SAMPLE_ASSETS)}/Models/{name}/{screenshot}"></td>'
+            report += f'<td><img src="{str(_RESULTS_PATH)}/{name}-{variant_name}/screenshot_frame_59.png"></td>'
+            report += '</tr>'
+
+    report += '</tbody>'
+    report += '</table></body></html>'
+
+    path.write_text(report)
+
+
+
 class GltfSampleAssetsTestCase(unittest.TestCase):
     def _subtest_model(self, model_spec):
         """Open all variants of a model
@@ -50,6 +81,11 @@ class GltfSampleAssetsTestCase(unittest.TestCase):
 
             self.assertEqual(process.returncode, 0)
 
+            # Make a PNG for the report (consider doing this outside the test)
+            subprocess.run(
+                ['convert', 'screenshot_frame_59.ppm', 'screenshot_frame_59.png'],
+                cwd=subtest_results_path)
+
     def test_all(self):
         """Opens all models in glTF-Sample-Assets.
         
@@ -64,6 +100,8 @@ class GltfSampleAssetsTestCase(unittest.TestCase):
             with self.subTest(model_spec):
                 self._subtest_model(model_spec)
         
+        _make_report(_RESULTS_PATH / 'report.html', models_index)
+
 
 if __name__ == '__main__':
     unittest.main()
